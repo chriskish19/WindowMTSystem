a61d92a45fe2816b5ab11f3cf64eee3528f23da0
chris hopkins <chriskish19@gmail.com>
2023-11-04 21:54:42 -0600

    Add project files.

---
+++ v1.0/main.cpp
@@ -0,0 +1,7 @@
+#include "iWindow.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
+	WMTS::MTPlainWin32Window Window;
+	Window.ExecuteThreads(2);
+	return 0;
+}
